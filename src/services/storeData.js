f28c74cad79e7541aeae5d0adddd86ab52158a3b
vinsensius.ferdinando <vinsensius.ferdinando@ui.ac.id>
2024-05-14 09:12:38 +0700

    fix bug

--- src/services/storeData.js
+++ src/services/storeData.js
@@ -1,13 +1,9 @@
 const { Firestore } = require('@google-cloud/firestore');
 
 async function storeData(id, data) {
-  console.log('TES MASUK storeData');
   const db = new Firestore();
-  console.log('Buat Firestore');
 
-  const predictCollection = db.collection('prediction');
-  console.log('Buat koleksi prediction');
-  console.log(predictCollection.doc(id).set(data));
+  const predictCollection = db.collection('predictions');
   return predictCollection.doc(id).set(data);
 }
 
